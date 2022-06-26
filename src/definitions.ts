9de311407ab15ab1d8b92d27062e7c06d5f4d9d1
Aparajita <aparajita@aparajita.com>
2022-06-26 14:30:10 -0700

    feat: support all types of plugin calls

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,13 +1,27 @@
-import { PluginResultError } from '@capacitor/core';
+import type { PluginResultError } from '@capacitor/core'
 
 /**
  * The options that are passed to a native plugin.
  */
-export type CallOptions = { [key: string]: any };
+export type CallOptions = Record<string, never>
 
 /**
  * Capacitor plugins may also return an error code.
  */
 export interface PluginError extends PluginResultError {
-  code?: string;
+  code?: string
+}
+
+/**
+ * The type of plugin call.
+ */
+
+export enum PluginReturnType {
+  none,
+  promise,
+  callback
+}
+
+export interface DecoratedNativePlugin {
+  getRegisteredPluginName: () => string
 }
--- src/native-decorator.ts
+++ src/native-decorator.ts
@@ -1,80 +1,170 @@
 /**
- * A decorator that converts a JavaScript method into a native call.
+ * A decorator that converts a JavaScript class method into a native call.
  */
-import { CallOptions, PluginError } from './definitions';
-import { Capacitor, PluginResultData, WebPlugin } from '@capacitor/core';
+import type { PluginCallback, PluginResultData } from '@capacitor/core'
+import { Capacitor } from '@capacitor/core'
+import type { CapacitorInstance } from '@capacitor/core/types/definitions-internal'
+import type { CallOptions, PluginError } from './definitions'
+import { PluginReturnType } from './definitions'
 
-const nativeMethodsProperty = '__native_methods__';
-let nativeMethods: Set<string>;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type Func = (...args: any[]) => any
 
-export function native() {
-  return function (
-    target: any,
-    methodName: string,
-    descriptor: PropertyDescriptor,
-  ) {
-    // target is the class prototype. If the class does not yet
-    // have a __native_methods__ property, add it now.
-    const targetClass = target.constructor;
+type WrappedFunction = (
+  optionsOrCallback?: CallOptions | PluginCallback,
+  callback?: PluginCallback
+) => Promise<unknown>
 
-    if (!targetClass.hasOwnProperty(nativeMethodsProperty)) {
-      // We'll use a set to easily ensure that no matter how
-      // many times the plugin is instantiated, the class property
-      // will only contain the set of unique native method names.
-      nativeMethods = new Set();
+interface DecoratedClassPrototype {
+  getRegisteredPluginName: () => string
+}
+/* eslint-enable */
 
-      Object.defineProperty(targetClass, nativeMethodsProperty, {
-        configurable: false,
-        enumerable: false,
-        writable: false,
-        value: nativeMethods,
-      });
-    }
-
-    nativeMethods.add(methodName);
-
-    const originalMethod = descriptor.value;
-
-    descriptor.value = function (options?: CallOptions) {
-      if (Capacitor.isNative) {
-        return callNativeMethod(this as WebPlugin, methodName, options);
-      } else {
-        return originalMethod.call(this, options);
-      }
-    };
-
-    return descriptor;
-  };
+function isFunction(value: unknown): value is Func {
+  return typeof value === 'function'
 }
 
-function callNativeMethod<T extends WebPlugin>(
-  plugin: T,
+async function callNativePromise(
+  name: string,
   methodName: string,
-  options?: CallOptions,
-): Promise<any> {
+  options?: CallOptions
+): Promise<unknown> {
   return new Promise((resolve, reject) => {
-    const resolver = (data?: PluginResultData) => {
+    const resolver = (data: PluginResultData | null): void => {
       // If there is only one property in data, return it bare
       if (data) {
-        const keys = Object.keys(data);
+        const keys = Object.keys(data)
 
         if (keys.length === 1) {
-          return resolve(data[keys[0]]);
+          resolve(data[keys[0]])
+          return
         }
       }
 
-      resolve(data);
-    };
+      resolve(data)
+    }
 
-    // @ts-ignore - toNative() is only defined in native environments,
-    // and this function is only called in native environments.
-    Capacitor.toNative(plugin.config.name, methodName, options, {
-      resolve: (data?: PluginResultData) => {
-        resolver(data);
-      },
-      reject: (error: PluginError) => {
-        reject(error);
-      },
-    });
-  });
+    // Capacitor is actually of type CapacitorInstance
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const cap = Capacitor as CapacitorInstance
+
+    cap
+      .nativePromise<CallOptions, PluginResultData | null>(
+        name,
+        methodName,
+        options
+      )
+      .then((data: PluginResultData | null) => {
+        resolver(data)
+      })
+      .catch((error: PluginError) => {
+        reject(error)
+      })
+  })
+}
+
+function wrappedFunction(
+  pluginName: string,
+  methodName: string,
+  returnType: PluginReturnType,
+  originalMethod: Func
+): WrappedFunction {
+  return async function (
+    optionsOrCallback?: CallOptions | PluginCallback,
+    callback?: PluginCallback
+  ): Promise<unknown> {
+    let options: CallOptions | undefined
+    let cb: PluginCallback | undefined
+
+    if (typeof optionsOrCallback === 'object') {
+      options = optionsOrCallback
+
+      if (typeof callback === 'function') {
+        cb = callback
+      }
+    } else if (typeof optionsOrCallback === 'function') {
+      cb = optionsOrCallback
+    }
+
+    if (Capacitor.isNativePlatform()) {
+      if (returnType === PluginReturnType.promise) {
+        return callNativePromise(pluginName, methodName, options)
+      } else {
+        // Capacitor is actually of type CapacitorInstance
+        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+        const cap = Capacitor as CapacitorInstance
+
+        return Promise.resolve(
+          cap.nativeCallback<CallOptions>(pluginName, methodName, options, cb)
+        )
+      }
+    } else {
+      // In the context of an instance method call
+      // (which is what got us here), `this` is the instance.
+      /* eslint-disable @typescript-eslint/no-unsafe-return,@typescript-eslint/no-invalid-this */
+      if (options) {
+        if (cb) {
+          // @ts-expect-error: See above comment
+          return originalMethod.call(this, options, cb)
+        }
+
+        // @ts-expect-error: See above comment
+        return originalMethod.call(this, options)
+      }
+
+      if (cb) {
+        // @ts-expect-error: See above comment
+        return originalMethod.call(this, cb)
+      }
+
+      // @ts-expect-error: See above comment
+      return originalMethod.call(this)
+    }
+  }
+}
+
+export function native(
+  returnType: PluginReturnType = PluginReturnType.promise
+) {
+  return function (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/explicit-module-boundary-types
+    target: any,
+    methodName: string,
+    descriptor: PropertyDescriptor
+  ): PropertyDescriptor {
+    if (isFunction(descriptor.value)) {
+      const originalMethod = descriptor.value
+      let prototype: DecoratedClassPrototype
+
+      //  We only support instance methods, that's what Capacitor supports.
+      //  If target is an instance method, target is an object.
+      if (typeof target === 'object') {
+        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+        prototype = target as DecoratedClassPrototype
+      } else {
+        throw new Error('@native can only be used with instance class methods')
+      }
+      /* eslint-enable */
+
+      // The class has to implement the instance method
+      // getRegisteredPluginName().
+      if (typeof prototype.getRegisteredPluginName === 'function') {
+        const pluginName = prototype.getRegisteredPluginName()
+        descriptor.value = wrappedFunction(
+          pluginName,
+          methodName,
+          returnType,
+          originalMethod
+        )
+      } else {
+        throw new Error(
+          'Classes that use @native must implement the method getRegisteredPluginName(): string'
+        )
+      }
+    } else {
+      throw new Error('@native can only be used with instance class methods')
+    }
+
+    return descriptor
+  }
 }
