17dd6e1564c64cc8d5d58322cd75bce2a84deb87
Aparajita <aparajita@aparajita.com>
2022-07-06 14:19:33 -0700

    docs: note that none will not reject

--- src/definitions.ts
+++ src/definitions.ts
@@ -15,10 +15,24 @@ export interface PluginError extends PluginResultError {
 /**
  * The type of plugin call.
  */
-
 export enum PluginReturnType {
+  /**
+   * The plugin call returns no data **and** will never reject.
+   * If you return no data but might reject, use `PluginReturnType.promise`,
+   * otherwise the promise on the TypeScript side will not reject.
+   */
   none,
+
+  /**
+   * The plugin call returns data and/or it might reject.
+   */
   promise,
+
+  /**
+   * The plugin call is passing a callback to be called repeatedly.
+   * The native plugin will mark the call `keepAlive` and will
+   * repeatedly resolve.
+   */
   callback
 }
 
